25bc085f12fb30321c9140beaa17c76618c6b057
JanetGilbert <janet@wadjeteyegames.com>
2021-03-24 15:54:25 -0400

    Added rigidbody controller

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Func<int, int, int> scoreLambda = (score, toAdd) =>
+        {
+            score += toAdd;
+
+            return score;
+        };
+
+        int curScore = 100;
+        Debug.Log(scoreLambda(curScore, 100));
+    }
+}
